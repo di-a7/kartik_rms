3efef7935abc4935078ee04069e4f0bd12d45f55
diya <shresthadiya59@gmail.com>
2025-12-31 16:57:00 +0545

    admin customize, postgresql database integration

--- rms/admin.py
+++ rms/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+   list_display = ['id','name']
+
+admin.site.register(Food)
+
+@admin.register(Table)
+class TableAdmin(admin.ModelAdmin):
+   list_display = ['number','capacity','avaiable']
+   list_filter = ['capacity','avaiable']
+   list_editable = ['avaiable']
+
+admin.site.register(Order)
+admin.site.register(OrderItem)   
