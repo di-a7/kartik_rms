c3b8c80912fbe279f558913ae3c377e9e13205b7
diya <shresthadiya59@gmail.com>
2026-01-01 16:57:52 +0545

    data seeding, admin customization, inline

--- rms/admin.py
+++ rms/admin.py
@@ -5,7 +5,6 @@ from .models import *
 class CategoryAdmin(admin.ModelAdmin):
    list_display = ['id','name']
 
-admin.site.register(Food)
 
 @admin.register(Table)
 class TableAdmin(admin.ModelAdmin):
@@ -13,5 +12,26 @@ class TableAdmin(admin.ModelAdmin):
    list_filter = ['capacity','avaiable']
    list_editable = ['avaiable']
 
-admin.site.register(Order)
-admin.site.register(OrderItem)   
+@admin.register(Food)
+class FoodAdmin(admin.ModelAdmin):
+   list_display = ['name','description','price','category']
+   list_filter = ['category']
+   search_fields = ['name']
+
+
+# class OrderItemInline(admin.StackedInline):
+class OrderItemInline(admin.TabularInline):
+   model = OrderItem
+   autocomplete_fields = ['food']
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+   list_display = ['id','user','table','total_price','status','payment_status']
+   list_filter = ['status','payment_status']
+   search_fields = ['user__username','table__number']
+   inlines = [OrderItemInline]
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+   list_display = ['order','food']
+   search_fields = ['food__name']
--- rms/models.py
+++ rms/models.py
@@ -4,17 +4,23 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 class Category(models.Model):
    name = models.CharField(max_length=100)
-
+   
+   def __str__(self):
+      return self.name
 class Food(models.Model):
    name = models.CharField(max_length=100)
    description = models.TextField()
    price = models.IntegerField()
    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+   def __str__(self):
+      return self.name
 
 class Table(models.Model):
    number = models.CharField(max_length=1)
    capacity = models.IntegerField()
    avaiable = models.BooleanField(default=False)
+   def __str__(self):
+      return f"Table {self.number}"
 
 class Order(models.Model):
    PENDING = "P"
@@ -30,6 +36,9 @@ class Order(models.Model):
    total_price = models.IntegerField(null=True, blank=True)
    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default = PENDING)
    payment_status = models.BooleanField(default = False)
+   
+   def __str__(self):
+      return f"{self.user} - {self.table}"
 
 
 class OrderItem(models.Model):
